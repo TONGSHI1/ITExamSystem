ef7d4e1aa014c3451a38b0e644c7449e9b01ded8
TONGSHI1 <mengchibang@qq.com>
2017-07-09 16:03:37 +0800

    登录注册 差到爆炸的版本 本来想用primeng加一个职业的下拉框，可是。。。先承认错误，回来一定好好写。我去收拾行李了。哭哭

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {DropdownModule} from 'primeng/primeng';
 import { AppComponent } from './app.component';
+import {LoginComponent, MyModel} from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MyModel,
   ],
   imports: [
     BrowserModule
